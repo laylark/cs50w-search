08162f967bc73c2e01ce68a2f26e1dd22c2212e7
laylark <laylan@gmail.com>
2022-10-30 00:35:33 -0500

    Add advanced-search page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function search(event) {
   event.preventDefault();
+  const path = window.location.pathname;
   const q = document.getElementById("q").value;
-  return window.location.assign(
-    "https://www.google.com/search?q=" + q + "&tbm=isch"
-  );
+  const urlParam = "";
+  //   const all = document.getElementById("all").value;
+  //   const exact = document.getElementById("exact").value;
+  //   const none = document.getElementById("none").value;
+
+  if (path == "/image-search.html") {
+    return window.location.assign(
+      "https://www.google.com/search?q=" + q + "&tbm=isch"
+    );
+  }
 }
