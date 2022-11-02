efb7f5653d8a3c841074f4ca492380bdcb69e9e6
laylark <laylan@gmail.com>
2022-11-01 20:57:47 -0500

    Implement advanced search modifiers

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 function search(event) {
   event.preventDefault();
   const path = window.location.pathname;
-  const q = document.getElementById("q").value;
-  const urlParam = "";
-  //   const all = document.getElementById("all").value;
-  //   const exact = document.getElementById("exact").value;
-  //   const none = document.getElementById("none").value;
 
   if (path == "/image-search.html") {
+    const q = document.getElementById("q").value;
     return window.location.assign(
       "https://www.google.com/search?q=" + q + "&tbm=isch"
+    );
+  }
+  if (path == "/advanced-search.html") {
+    const all = document.getElementById("all").value;
+    const exact = document.getElementById("exact").value;
+    const any = document.getElementById("any").value;
+    const none = document.getElementById("none").value;
+
+    return window.location.assign(
+      "https://www.google.com/search?as_q=" +
+        all +
+        "&as_epq=" +
+        exact +
+        "&as_oq=" +
+        any +
+        "&as_eq=" +
+        none
     );
   }
 }
