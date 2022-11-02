a1dd41d02dcd6246a55daa8247d650de6d3da366
laylark <laylan@gmail.com>
2022-11-01 21:55:54 -0500

    Implement html form submission for image search

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 function search(event) {
   event.preventDefault();
-  const path = window.location.pathname;
+  const all = document.getElementById("all").value;
+  const exact = document.getElementById("exact").value;
+  const any = document.getElementById("any").value;
+  const none = document.getElementById("none").value;
 
-  if (path == "/image-search.html") {
-    const q = document.getElementById("q").value;
-    return window.location.assign(
-      "https://www.google.com/search?q=" + q + "&tbm=isch"
-    );
-  }
-  if (path == "/advanced-search.html") {
-    const all = document.getElementById("all").value;
-    const exact = document.getElementById("exact").value;
-    const any = document.getElementById("any").value;
-    const none = document.getElementById("none").value;
-
-    return window.location.assign(
-      "https://www.google.com/search?as_q=" +
-        all +
-        "&as_epq=" +
-        exact +
-        "&as_oq=" +
-        any +
-        "&as_eq=" +
-        none
-    );
-  }
+  return window.location.assign(
+    "https://www.google.com/search?as_q=" +
+      all +
+      "&as_epq=" +
+      exact +
+      "&as_oq=" +
+      any +
+      "&as_eq=" +
+      none
+  );
 }
